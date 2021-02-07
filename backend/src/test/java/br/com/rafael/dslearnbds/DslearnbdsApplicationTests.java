f20f5ee4258f64115fb99172c546c5377571f6a8
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2021-02-06 21:15:53 -0300

    Backend project created

---
+++ backend/src/test/java/br/com/rafael/dslearnbds/DslearnbdsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafael.dslearnbds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnbdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
